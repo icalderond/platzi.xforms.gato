528f1f522e570818fe28d7a0a740d31ece453be6
icalderond <icalderond@outlook.com>
2019-03-04 23:03:29 -0600

    Gato finished for practice on course

---
+++ platzi.xforms.gato/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace platzi.xforms.gato
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Tapped(object sender, System.EventArgs e)
+        {
+            var currentGrid = ((Grid)sender);
+
+            if (currentGrid.Children.Any())
+            {
+                currentGrid.Children.Clear();
+            }
+            else
+            {
+                BoxView boxView = new BoxView
+                {
+                    WidthRequest = 50,
+                    HeightRequest = 50,
+                    BackgroundColor = Color.Magenta,
+                    VerticalOptions = LayoutOptions.Center,
+                    HorizontalOptions = LayoutOptions.Center,
+                    CornerRadius = 25
+                };
+
+                currentGrid.Children.Add(boxView);
+            }
+        }
+    }
+}
